e514e9d0851f69a04289efc85e1f4cc16d3f1f35
adriro20 <adrianrochaasir@gmail.com>
2024-10-29 20:15:04 +0100

    aniadida funcionalidad a main_activity

--- Reto/app/src/main/java/com/example/reto/MainActivity.java
+++ Reto/app/src/main/java/com/example/reto/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.reto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TableLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSalir = null;
+    Button btnAnadir = null;
+    Button btnVer = null;
+    Spinner comboGrupoMusc = null;
+    TableLayout tablaEjercicios = null;
+    private final int anadirActivity = 1;
+    private final int grupoActivity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnSalir = (Button) findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnAnadir = (Button) findViewById(R.id.btnAnadir);
+        btnAnadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CrearEjercicioActivity.class);
+                startActivityForResult(intent, anadirActivity);
+            }
+        });
+        btnVer = (Button) findViewById(R.id.btnVer);
+        btnVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GrupoMuscularArctivity.class);
+                startActivityForResult(intent, grupoActivity);
+            }
+        });
+        comboGrupoMusc = (Spinner) findViewById(R.id.comboGrupoMusc);
+        tablaEjercicios = (TableLayout) findViewById(R.id.tablaEjercicios);
     }
 }
