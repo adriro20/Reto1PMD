e1030b29eb67796410b3f007129bc5c1d539cb86
2dam <2dam@TartangaLH.eus>
2024-10-30 09:35:04 +0100

    cambio en la BD y creada funcionalidad en la ventana Crear

--- Reto/app/src/main/java/com/example/reto/CrearEjercicioActivity.java
+++ Reto/app/src/main/java/com/example/reto/CrearEjercicioActivity.java
@@ -1,6 +1,10 @@
 package com.example.reto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CrearEjercicioActivity extends AppCompatActivity {
+
+    private Ejercicio ejercicio;
+
+    private EditText etNombre;
+    private EditText etGrupo;
+    private EditText etSeries;
+    private EditText etRepeticiones;
+    private EditText etDescripcion;
+
+    private Button btnSalir;
+    private Button btnVolver;
+    private Button btnCrear;
+
+    private DBAccess dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +40,47 @@ public class CrearEjercicioActivity extends AppCompatActivity {
         });
 
 
+        etNombre = findViewById(R.id.etNombre);
+        etGrupo = findViewById(R.id.etGrupo);
+        etSeries = findViewById(R.id.etSeries);
+        etRepeticiones = findViewById(R.id.etRepeticiones);
+        etDescripcion = findViewById(R.id.etDescripcion);
+
+        btnSalir = findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(this::cerrarApp);
+
+        btnVolver = findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(this::volverAtras);
+
+        btnCrear = findViewById(R.id.btnCrear);
+        btnCrear.setOnClickListener(this::crearEjercicio);
+
+        Intent intent = getIntent();
+        ejercicio = (Ejercicio) intent.getSerializableExtra("EJERCICIO");
+        
+    }
+
+    private void crearEjercicio(View view) {
+        try{
+            etNombre.setText(ejercicio.getNombre());
+            etGrupo.setText(ejercicio.getGrupo());
+            etSeries.setText(ejercicio.getSeries());
+            etRepeticiones.setText(ejercicio.getRepeticiones());
+            etDescripcion.setText(ejercicio.getDescripcion());
+
+            dao.setEjercicio(ejercicio);
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void cerrarApp(View view) {
+        finishAffinity();
+    }
+
+    private void volverAtras(View view){
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- Reto/app/src/main/java/com/example/reto/DBAccess.java
+++ Reto/app/src/main/java/com/example/reto/DBAccess.java
@@ -11,15 +11,22 @@ import java.util.Map;
 public class DBAccess implements DBAccesible {
 
     private SQLiteDatabase dataBase;
-    private String crearTablas = "CREATE TABLE IF NOT EXISTS ejercicio (nombre varchar(30) PRIMARY KEY, grupo varchar(30), descripcion Text, repeticiones Integer, series Integer, imagen Text, video Text, audio Text);";
+
+    private String crearTablaGrupo = "CREATE TABLE IF NOT EXISTS grupo (id Integer PRIMARY KEY, nombre varchar(30) NOT NULL)";
+    private String crearTablaEjercicio = "CREATE TABLE IF NOT EXISTS ejercicio (nombre varchar(30) PRIMARY KEY, idGrupo Integer, descripcion Text, repeticiones Integer, series Integer, imagen Text, video Text, audio Text, FOREIGN KEY (idGrupo) REFERENCES grupo(id) ON DELETE CASCADE);";
+    private String selectIdGrupo = "SELECT id FROM grupo WHERE nombre = ?";
+    private String selectNombreGrupo = "SELECT nombre FROM grupo WHERE id = ?";
+    private String selectGrupos = "SELECT * FROM grupo";
     private String select = "SELECT * FROM ejercicio";
-    private String insert = "INSERT INTO ejercicio (nombre, grupo, descripcion, repeticiones, series, imagen, video, audio) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    private String insertGrupos = "INSERT INTO grupo(nombre) VALUES ('brazo'),('pierna'),('pecho'),('espalda')";
+    private String insertEjercicio = "INSERT INTO ejercicio (nombre, idGrupo, descripcion, repeticiones, series, imagen, video, audio) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     public DBAccess(Context context) {
         try {
             dataBase = context.openOrCreateDatabase("BDEjercicios", Context.MODE_PRIVATE, null);
-
-            dataBase.execSQL(crearTablas);
+            dataBase.execSQL(crearTablaGrupo);
+            dataBase.execSQL(crearTablaEjercicio);
+            dataBase.execSQL(insertGrupos);
 
         } catch (SQLiteException e) {
             Toast.makeText(context, R.string.txtErrorCrearBD, Toast.LENGTH_LONG).show();
@@ -27,21 +34,53 @@ public class DBAccess implements DBAccesible {
     }
 
     @Override
-    public Map<String, Ejercicio> selectEjercicio(){
+    public Map<Integer, String> getGrupos() {
+        Map<Integer, String> grupos = new HashMap<>();
+        Cursor cursor = null;
+        try{
+            cursor = dataBase.rawQuery(selectGrupos, null);
+            if(cursor.getCount() != 0){
+                while (cursor.moveToNext()){
+                    grupos.put(Integer.parseInt(cursor.getString(0)),cursor.getString(1));
+                }
+            }
+        }catch(SQLiteException e){
+            e.printStackTrace();
+        }finally {
+            if(cursor != null){
+                cursor.close();
+            }
+
+        }
+
+        return grupos;
+    }
+
+    @Override
+    public Map<String, Ejercicio> getEjercicios(){
         Map<String, Ejercicio> ejercicios = new HashMap<>();
-        Cursor cursor = dataBase.rawQuery(select, null);
-        if(cursor.getCount() != 0){
-            while(cursor.moveToNext()){
-                Ejercicio ejer = new Ejercicio();
-                ejer.setNombre(cursor.getString(0));
-                ejer.setGrupo(cursor.getString(1));
-                ejer.setDescripcion(cursor.getString(2));
-                ejer.setRepeticiones(Integer.parseInt(cursor.getString(3)));
-                ejer.setSeries(Integer.parseInt(cursor.getString(4)));
-                ejer.setImagen(cursor.getString(5));
-                ejer.setVideo(cursor.getString(6));
-                ejer.setAudio(cursor.getString(7));
-                ejercicios.put(ejer.getNombre(),ejer);
+        Cursor cursor = null;
+        try{
+            cursor = dataBase.rawQuery(select, null);
+            if(cursor.getCount() != 0){
+                while(cursor.moveToNext()){
+                    Ejercicio ejer = new Ejercicio();
+                    ejer.setNombre(cursor.getString(0));
+                    ejer.setGrupo(getNombreGrupo(Integer.parseInt(cursor.getString(1))));
+                    ejer.setDescripcion(cursor.getString(2));
+                    ejer.setRepeticiones(Integer.parseInt(cursor.getString(3)));
+                    ejer.setSeries(Integer.parseInt(cursor.getString(4)));
+                    ejer.setImagen(cursor.getString(5));
+                    ejer.setVideo(cursor.getString(6));
+                    ejer.setAudio(cursor.getString(7));
+                    ejercicios.put(ejer.getNombre(),ejer);
+                }
+            }
+        }catch(SQLiteException e){
+            e.printStackTrace();
+        }finally {
+            if(cursor != null){
+                cursor.close();
             }
         }
 
@@ -49,10 +88,10 @@ public class DBAccess implements DBAccesible {
     }
 
     @Override
-    public void insertEjercicio(Ejercicio ejer){
-        dataBase.execSQL(insert, new Object[]{
+    public void setEjercicio(Ejercicio ejer){
+        dataBase.execSQL(insertEjercicio, new Object[]{
                 ejer.getNombre(),
-                ejer.getGrupo(),
+                getIdGrupo(ejer.getGrupo()),
                 ejer.getDescripcion(),
                 ejer.getRepeticiones(),
                 ejer.getSeries(),
@@ -61,8 +100,47 @@ public class DBAccess implements DBAccesible {
                 ejer.getAudio()
         });
 
-
     }
 
+    private Integer getIdGrupo(String grupo){
+        Integer id = null;
+        Cursor cursor = null;
+        try{
+            cursor = dataBase.rawQuery(selectIdGrupo, new String[]{grupo});
+            if(cursor.getCount() != 0){
+                cursor.moveToNext();
+                id = Integer.parseInt(cursor.getString(0));
+            }
+        }catch(SQLiteException e){
+            e.printStackTrace();
+        }finally {
+            if(cursor != null){
+                cursor.close();
+            }
+
+        }
+
+        return id;
+    }
+
+    private String getNombreGrupo(Integer id){
+        String nombre = null;
+        Cursor cursor = null;
+        try{
+            cursor = dataBase.rawQuery(selectNombreGrupo, new String[]{String.valueOf(id)});
+            if(cursor.getCount() != 0){
+                cursor.moveToNext();
+                nombre = cursor.getString(0);
+            }
+        }catch(SQLiteException e){
+            e.printStackTrace();
+        }finally {
+            if(cursor != null){
+                cursor.close();
+            }
+
+        }
+        return nombre;
+    }
 
 }
